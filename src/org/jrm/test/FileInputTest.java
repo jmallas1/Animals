355011da366eb783386dca85f941fc841ac33852
Jared R. Mallas <jrtytn@gmail.com>
2018-08-27 19:38:27 -0500

    junits for fileInput

--- src/org/jrm/test/FileInputTest.java
+++ src/org/jrm/test/FileInputTest.java
@@ -1,32 +1,39 @@
 package org.jrm.test;
 
+import org.jrm.FileInput;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/* TODO: Write tests for fileInput */
-
 class FileInputTest {
+
+    private FileInput fi;
 
     @BeforeEach
     void setUp() {
+        fi = new FileInput("./src/org/jrm/test/FileInputTest.java");
     }
 
     @AfterEach
     void tearDown() {
     }
-
+/*
     @Test
     void fileRead() {
+        // Not sure how to test reading a whole file...
     }
-
+*/
     @Test
     void fileReadLine() {
+        assertEquals("package org.jrm.test;", fi.fileReadLine());
     }
 
-    @Test
+/*    @Test
     void fileClose() {
+        // How to test a closed file without throwing an exception?
     }
+*/
+
 }
