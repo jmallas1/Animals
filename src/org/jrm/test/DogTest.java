485d1457029e950715585ba106205c8e8ec76232
Jared R. Mallas <jrtytn@gmail.com>
2018-08-27 19:11:17 -0500

    junits for Dawg

--- src/org/jrm/test/DogTest.java
+++ src/org/jrm/test/DogTest.java
@@ -1,32 +1,41 @@
 package org.jrm.test;
 
+import org.jrm.Dog;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/* TODO: write tests for dawg */
-
 class DogTest {
+
+    private Dog someFriendlyDog;
+    private Dog someMeanDog;
 
     @BeforeEach
     void setUp() {
+        someFriendlyDog = new Dog(true, "Trooper");
+        someMeanDog = new Dog(false, "Fluffy");
     }
 
     @AfterEach
-    void tearDown() {
-    }
+    void tearDown() { someFriendlyDog = null; }
 
     @Test
     void isFriendly() {
+        assertTrue(someFriendlyDog.isFriendly());
+        assertFalse(someMeanDog.isFriendly());
     }
 
     @Test
     void talk() {
+        assertEquals("Bark", someFriendlyDog.talk());
+        assertEquals("Bark", someMeanDog.talk());
     }
 
     @Test
     void getName() {
+        assertEquals("Trooper", someFriendlyDog.getName());
+        assertEquals("Fluffy", someMeanDog.getName());
     }
 }
